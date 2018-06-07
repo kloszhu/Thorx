fb2c0997494398de0fa53e091a7379cdeb20202c
朱雄 <314963663@qq.com>
2018-06-08 01:10:40 +0800

    增加web扩展

---
+++ Thorx.Web.Extention/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Thorx.Web.Extention
+{
+    public class MongoController<T> : Controller
+    {
+
+    }
+}
